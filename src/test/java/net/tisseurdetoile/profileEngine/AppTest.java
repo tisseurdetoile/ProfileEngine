2a5d38276f1614a45b78018ae68e2d270bd6bb6f
TisseurDetoile <webmaster@tisseurdetoile.net>
2014-12-10 14:48:41 +0100

    modification des tests

--- src/test/java/net/tisseurdetoile/profileEngine/AppTest.java
+++ src/test/java/net/tisseurdetoile/profileEngine/AppTest.java
@@ -10,56 +10,96 @@ import net.tisseurdetoile.profileEngine.specifications.hashmap.AttributeValueIn;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest
+        extends TestCase {
+
+    ISpecification<HashMap<String, String>> sp11 = new AttributeValueIn("nom", "nomdetest");
+    ISpecification<HashMap<String, String>> sp12 = new AttributeValueIn("ville", "villedetest");
+    ISpecification<HashMap<String, String>> sp21 = new AttributeValueIn("nom", "nomdetest2");
+    ISpecification<HashMap<String, String>> sp22 = new AttributeValueIn("ville", "villedetest2");
+    ISpecification<HashMap<String, String>> sp1X = sp11.and(sp12);
+    ISpecification<HashMap<String, String>> sp2X = sp21.and(sp22);
+
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    public HashMap<String, String> getUser1() {
+        HashMap<String, String> testData = new HashMap<String, String>();
+
+        testData.put("nom", "nomdetest");
+        testData.put("prenom", "prenomdetest");
+        testData.put("ville", "villedetest");
+
+        return testData;
+    }
+
+    public HashMap<String, String> getUser2() {
+        HashMap<String, String> testData = new HashMap<String, String>();
+
+        testData.put("nom", "nomdetest2");
+        testData.put("prenom", "prenomdetest2");
+        testData.put("ville", "villedetest2");
+
+        return testData;
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        HashMap<String,String> testData = new HashMap<String,String>();
+    public void testSP2U1() {
+        ISpecification<HashMap<String, String>> sptest2 = sp11.and(sp12).or(sp21).and(sp22);
+        assertEquals("sptest2 avec user1", false, sptest2.isSatisfiedBy(getUser1()));
 
+    }
 
-        testData.put("nom", "nomdetest");
-        testData.put("prenom", "prenomdetest");
-        testData.put("ville", "villedetest");
-        testData.put("param1", "param1detest");
-        testData.put("param2", "param2detest");
-        testData.put("param3", "param3detest");
+    public void testSP2U2() {
+        ISpecification<HashMap<String, String>> sptest2 = sp11.and(sp12).or(sp21).and(sp22);
+        assertEquals("sptest2 avec user2", true, sptest2.isSatisfiedBy(getUser2()));
+    }
 
+    public void testSP1U1() {
+        ISpecification<HashMap<String, String>> sptest = sp1X.or(sp2X);
+        assertEquals("sptest avec user1", true, sptest.isSatisfiedBy(getUser1()));
 
-        ISpecification<HashMap<String,String>> sp1 = new AttributeValueIn("nom", "nomdetest");
-        ISpecification<HashMap<String,String>> sp2 = new AttributeValueIn("prenom", "prenomdetest");
-        ISpecification<HashMap<String,String>> sp3 = new AttributeValueIn("ville", "villedetest");
-        ISpecification<HashMap<String,String>> sp4 = new AttributeValueIn("param1", "param1detest");
-        ISpecification<HashMap<String,String>> sp5 = new AttributeValueIn("param2", "param2detest");
-        ISpecification<HashMap<String,String>> sp6 = new AttributeValueIn("param3", "param3detest");
+    }
 
+    public void testSP1U2() {
+        ISpecification<HashMap<String, String>> sptest = sp1X.or(sp2X);
+        assertEquals("sptest avec user2", true, sptest.isSatisfiedBy(getUser2()));
+    }
 
-        ISpecification<HashMap<String,String>> sp7 =  sp6.and(sp1);
+    public void test2SP1U1() {
+        ISpecification<HashMap<String, String>> sptest = sp1X.or(sp21.and(sp22));
+        assertEquals("sptest avec user1", true, sptest.isSatisfiedBy(getUser1()));
 
-        ISpecification<HashMap<String,String>> sp8 = sp7.or(sp1);
-        
+    }
 
+    public void test2SP1U2() {
+        ISpecification<HashMap<String, String>> sptest = sp1X.or(sp21.and(sp22));
+        assertEquals("sptest avec user2", true, sptest.isSatisfiedBy(getUser2()));
+    }
 
+    public void test3SP1U1() {
+        ISpecification<HashMap<String, String>> sptest = sp11.and(sp12).or(sp21.and(sp22));
+        assertEquals("sptest avec user1", true, sptest.isSatisfiedBy(getUser1()));
+
+    }
+
+    public void test3SP1U2() {
+        ISpecification<HashMap<String, String>> sptest = sp11.and(sp12).or(sp21.and(sp22));
+        assertEquals("sptest avec user2", true, sptest.isSatisfiedBy(getUser2()));
     }
 }
