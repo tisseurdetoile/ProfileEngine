092e5358fa1742cf7995a31d2afad26e0175b2b7
TisseurDetoile <webmaster@tisseurdetoile.net>
2014-12-10 14:49:31 +0100

    ajout des données

--- src/main/java/net/tisseurdetoile/profileEngine/App.java
+++ src/main/java/net/tisseurdetoile/profileEngine/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        /**
+         * "[IsValorisedAttribute:codeEmploi].AND[IsAttributeValueIn:codeEmploi:00182].OR[IsAttributeValueIn:codeEmploi:00183]"
+         * public static final String regExp = "(\\[([^]]+)\\](.(AND|OR|NOT)){0,1})";
+         */
     }
 }
