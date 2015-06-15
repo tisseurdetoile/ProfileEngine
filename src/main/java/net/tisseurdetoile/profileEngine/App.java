f361bdc06335de290bac2cc023d74b825960abb1
TisseurDeToile <webmaster@tisseurdetoile.net>
2015-06-15 20:21:41 +0200

    Ajout de jambon pour le monitoring

--- src/main/java/net/tisseurdetoile/profileEngine/App.java
+++ src/main/java/net/tisseurdetoile/profileEngine/App.java
@@ -1,9 +1,12 @@
 package net.tisseurdetoile.profileEngine;
 
+import com.jamonapi.Monitor;
+import com.jamonapi.MonitorFactory;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.logging.Logger;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
@@ -13,15 +16,26 @@ import org.apache.commons.csv.CSVRecord;
  */
 public class App {
 
+    private static final Logger LOG = Logger.getLogger("App");
+
     public static void main(String[] args) throws FileNotFoundException, IOException {
+        Monitor mon = MonitorFactory.start("myFirstMonitor");
+        Monitor monParse = MonitorFactory.start("parse");
+
         System.out.println("Hello World!");
 
         Reader in = new FileReader("./data/Teams.csv");
         Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
 
         for (CSVRecord record : records) {
+            monParse.start();
             System.out.println(record.toString());
+            monParse.stop();
         }
 
+        mon.stop();
+        monParse.stop();
+        System.out.println(mon);
+        System.out.println(monParse);
     }
 }
